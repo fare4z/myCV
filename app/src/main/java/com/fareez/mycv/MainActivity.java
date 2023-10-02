2a2e90800f0ce541c8909b0501e26b632b8adbb9
fare4z <fareez@psp.edu.my>
2023-10-02 13:21:10 +0800

    new

--- app/src/main/java/com/fareez/mycv/MainActivity.java
+++ app/src/main/java/com/fareez/mycv/MainActivity.java
@@ -1,9 +1,12 @@
 package com.fareez.mycv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +24,27 @@ TextView tvUsername;
         tvUsername.setText("Welcome " + Username);
 
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item ) {
+
+        switch (item.getItemId()){
+            case R.id.back:
+                Intent itnBack = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(itnBack);
+                break;
+            case R.id.logout:
+                Intent itnLogout = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(itnLogout);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
